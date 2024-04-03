321149ac87bed2372939b3f2fcebbfb4e1d78019
chan young <white1614@naver.com>
2024-04-03 21:35:59 +0900

    swiper 변경

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
-import Swiper from 'react-native-web-swiper';
+import { ActivityIndicator, Dimensions, StyleSheet, useColorScheme } from 'react-native';
+import Swiper from 'react-native-swiper';
 import styled from 'styled-components/native';
 import { makeImgPath } from '../utils';
 import { BlurView } from 'expo-blur';
@@ -8,7 +8,7 @@ import { BlurView } from 'expo-blur';
 const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Movie = () => {
-
+  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
 
@@ -38,16 +38,30 @@ const Movie = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{width: "100%", height: SCREEN_HEIGHT / 4}}
       >
         {nowPlaying.map(movie => (
           <View key={movie.id}>
             <BgImg style={StyleSheet.absoluteFill} source={{uri: makeImgPath(movie.backdrop_path)}}/>
-            <BlurView intensity={100} style={StyleSheet.absoluteFill}>
-              <Title>{movie.original_title}</Title>
+            <BlurView 
+              tint={isDark ? "dark" : "light"}
+              intensity={100} 
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{uri: makeImgPath(movie.poster_path)}}/>
+                <Column>
+                  <Title>{movie.original_title}</Title>
+                  <Overview>{movie.overview.slice(0, 80)}...</Overview>
+                  {movie.vote_average > 0 ? (<Votes>⭐️{movie.vote_average}/ 10</Votes>) : null}
+                </Column>
+              </Wrapper>
             </BlurView>
           </View>
         ))}
@@ -59,7 +73,7 @@ const Movie = () => {
 const Container = styled.ScrollView`
   background-color: ${props => props.theme.mainBgColor};
 `
- 
+
 const View = styled.View`
   flex: 1;
 `
@@ -73,8 +87,35 @@ const Loader = styled.View`
 const BgImg = styled.Image`
 `
 
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`
+
 const Title = styled.Text`
-  
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+`
+const Wrapper = styled.View`
+  height: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`
+
+const Overview = styled.Text`
+  margin-top: 10px;
+  color: rgba(255,255,255, 0.7);
+`
+
+const Votes = styled(Overview)`
+  font-size: 12px;
 `
 
 export default Movie;
